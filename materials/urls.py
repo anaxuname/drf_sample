b20ef6e88430a34bc23bcd902a42d4d291850b03
Anaxuname <anaxuname@gmail.com>
2024-02-15 15:01:22 +0300

    added viws

--- materials/urls.py
+++ materials/urls.py
@@ -1,6 +1,14 @@
 from materials.views import CourseViewSet
-from .apps import MaterialsConfig
 from rest_framework.routers import DefaultRouter
+from materials.apps import MaterialsConfig
+from django.urls import path
+from materials.views import (
+    LessonCreateAPIView,
+    LessonDestroyAPIView,
+    LessonListAPIView,
+    LessonUpdateAPIView,
+    LessonRetrieveAPIView,
+)
 
 app_name = MaterialsConfig.name
 
@@ -8,4 +16,10 @@ app_name = MaterialsConfig.name
 router = DefaultRouter()
 router.register(r"course", CourseViewSet, basename="course")
 
-urlpatterns = [] + router.urls
+urlpatterns = [
+    path("lesson/create/", LessonCreateAPIView.as_view(), name="lesson_create"),
+    path("lesson/delete/<int:pk>/", LessonDestroyAPIView.as_view(), name="lesson_delete"),
+    path("lesson/update/<int:pk>/", LessonUpdateAPIView.as_view(), name="lesson_update"),
+    path("lesson/<int:pk>/", LessonRetrieveAPIView.as_view(), name="lesson_detail"),
+    path("lesson/", LessonListAPIView.as_view(), name="lesson_list"),
+] + router.urls
