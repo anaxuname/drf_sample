6aad8495aa8c0ca3feec3565685d70479e6374fb
Anaxuname <anaxuname@gmail.com>
2024-02-18 23:12:51 +0300

    chenged settigs

--- materials/serializers.py
+++ materials/serializers.py
@@ -3,12 +3,32 @@ from materials.models import Course, Lesson
 
 
 class CourseSerializer(serializers.ModelSerializer):
+    count_lessons = serializers.SerializerMethodField()
+    list_lessons = serializers.SerializerMethodField()
+
+    def get_count_lessons(self, obj):
+        return Lesson.objects.filter(course=obj).count()
+
+    def get_list_lessons(self, obj):
+        return [lesson.name for lesson in Lesson.objects.filter(course=obj)]
+
     class Meta:
         model = Course
         fields = "__all__"
 
 
-class LessonsSerializer(serializers.ModelSerializer):
+class LessonSerializer(serializers.ModelSerializer):
+    # course_detail = CourseSerializer(read_only=True, many=True, source="course")
+    # count_lessons = serializers.SerializerMethodField()
+
+    # def get_count_lessons(self, obj):
+    #     return Lesson.objects.filter(course=obj).count()
+
     class Meta:
         model = Lesson
-        fields = ["name", "description", "link", "course"]
+        fields = [
+            "name",
+            "description",
+            "link",
+            "course",
+        ]
