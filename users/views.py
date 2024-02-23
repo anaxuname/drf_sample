28a109be66dc970ba2f7cca546a3c313a93e0b99
Anaxuname <anaxuname@gmail.com>
2024-02-23 18:39:08 +0300

    added rerialisers to users

--- users/views.py
+++ users/views.py
@@ -1,10 +1,10 @@
-from django.shortcuts import render
+# from django.shortcuts import render
 from rest_framework import generics
 from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework.filters import OrderingFilter
-from users.models import Payment
+from users.models import Payment, User
 
-from users.serializers import PaymentSerializer
+from users.serializers import PaymentSerializer, UserSerializer
 
 
 class PaymentListView(generics.ListAPIView):
@@ -13,3 +13,22 @@ class PaymentListView(generics.ListAPIView):
     filter_backends = [DjangoFilterBackend, OrderingFilter]
     ordering_fields = ["date"]
     filterset_fields = ["oblate_method", "paid_course", "paid_lesson"]
+
+
+class UserCreateView(generics.CreateAPIView):
+    serializer_class = UserSerializer
+
+
+class UserUpdateView(generics.UpdateAPIView):
+    serializer_class = UserSerializer
+    queryset = User.objects.all()
+
+
+class UserDeleteView(generics.DestroyAPIView):
+    serializer_class = UserSerializer
+    queryset = User.objects.all()
+
+
+class UserRetrieveView(generics.RetrieveAPIView):
+    serializer_class = UserSerializer
+    queryset = User.objects.all()
