4aa551fe91953ba2016ad2ba1720b2777a5862ac
Anaxuname <anaxuname@gmail.com>
2024-03-03 20:29:18 +0300

    subscribtion info in courses serializer

--- materials/serializers.py
+++ materials/serializers.py
@@ -1,11 +1,15 @@
 from rest_framework import serializers
-from materials.models import Course, Lesson
+from materials.models import Course, Lesson, Subscription
 from materials.validators import LinkValidator
 
 
 class CourseSerializer(serializers.ModelSerializer):
     count_lessons = serializers.SerializerMethodField()
     list_lessons = serializers.SerializerMethodField()
+    subscription = serializers.SerializerMethodField()
+
+    def get_subscription(self, obj):
+        return Subscription.objects.filter(user=self.context["request"].user, course=obj).exists()
 
     def get_count_lessons(self, obj):
         return Lesson.objects.filter(course=obj).count()
@@ -15,7 +19,7 @@ class CourseSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Course
-        fields = ["name", "preview_course", "description", "count_lessons", "list_lessons"]
+        fields = ["name", "preview_course", "description", "count_lessons", "list_lessons", "subscription"]
 
 
 class LessonSerializer(serializers.ModelSerializer):
