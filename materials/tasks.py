ca3bd47696f98af3c5c6f1eae960928d835341df
Anaxuname <anaxuname@gmail.com>
2024-03-07 02:10:09 +0300

    send email to subscribers task

---
+++ materials/tasks.py
@@ -0,0 +1,17 @@
+from celery import shared_task
+from django.urls import reverse_lazy
+from materials.models import Lesson, Subscription
+from django.core.mail import send_mail
+
+
+@shared_task
+def send_email_to_subscribers(lesson_pk, base_url):
+    lesson = Lesson.objects.get(pk=lesson_pk)
+    emails = [subscription.user.email for subscription in Subscription.objects.filter(course=lesson.course)]
+    send_mail(
+        "Новый урок!",
+        "Ссылка на урок: " + base_url + reverse_lazy("materials:lesson_retrieve", args=[lesson_pk]),
+        "from@example.com",
+        emails,
+        fail_silently=False,
+    )
