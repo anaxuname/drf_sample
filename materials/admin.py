cca9f874daa565c1e2be4ccdabeaa065b9ad7a0d
Anaxuname <anaxuname@gmail.com>
2024-03-07 01:22:24 +0300

    course and lesson admin config

--- materials/admin.py
+++ materials/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
 
+from materials.models import Course, Lesson
+
+
 # Register your models here.
+@admin.register(Course)
+class CourseAdmin(admin.ModelAdmin):
+    list_display = ("name", "price_rub", "user")
+
+
+@admin.register(Lesson)
+class LessonAdmin(admin.ModelAdmin):
+    list_display = ("name", "course", "link", "user")
